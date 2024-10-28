772393b7df1e1196edc61e7f880c61f00c89a300
renisha <133648889+Renieha@users.noreply.github.com>
2024-10-28 15:15:37 +0530

    Update FacebookRegistration.java

--- wee2assignment/FacebookRegistration.java
+++ wee2assignment/FacebookRegistration.java
@@ -25,24 +25,24 @@ public class FacebookRegistration {
 		//enter lastname field
 		driver.findElement(By.name("lastname")).sendKeys("rajendran");
 		//enter email id
-		driver.findElement(By.name("reg_email__")).sendKeys("sangameswarimr@gmail.com");
+		driver.findElement(By.name("reg_email__")).sendKeys("renisha20043@gmail.com");
 		//enter New password
-		driver.findElement(By.name("reg_passwd__")).sendKeys("raja@123");
+		driver.findElement(By.name("reg_passwd__")).sendKeys("reni43");
 		
 		//locate the dropdown element inthe date field
 		WebElement dateWE = driver.findElement(By.xpath("//select[@id='day']"));
 		//create object for Select and pass WebElement 
 		Select dateDD = new Select(dateWE);
 		//select the value
-		dateDD.selectByIndex(10);
+		dateDD.selectByIndex(8);
 		//locate and select the value in the month dropdown field
 		WebElement monthWE = driver.findElement(By.name("birthday_month"));
 		Select monthDD = new Select(monthWE);
-		monthDD.selectByIndex(0);
+		monthDD.selectByIndex(2);
 		// locate and select the value in the year dropdown field
 		WebElement yearWE = driver.findElement(By.name("birthday_year"));
 		Select yearDD =new Select(yearWE);
-		yearDD.selectByIndex(33);
+		yearDD.selectByIndex(60);
 		//locate and click female
 		driver.findElement(By.xpath("//label[text()='Female']")).click();
 		//(or) 
